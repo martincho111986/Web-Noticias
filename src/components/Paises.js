270b70fd4f872fc0d0f4181ccaa6db3a7774d65d
martin jerez <martincho111986@gmail.com>
2020-03-29 16:49:30 -0300

    pequeñas modificaciones

---
+++ src/components/Paises.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Paises = (pais) => {
+    return (
+        
+                <>
+                <option value='Argentina'>{pais.ar}</option>
+                </>
+            
+        
+
+
+    );
+}
+
+export default Paises;
